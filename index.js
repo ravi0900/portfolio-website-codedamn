6cca40371115d7f302611dd5bca2b0b46b317d94
Ravi <65914575+ravi0900@users.noreply.github.com>
2022-10-06 10:32:33 +0530

    hacktoberfest

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 AOS.init();
-
 const menu = document.querySelector(".menu");
 const navOpen = document.querySelector(".hamburger");
 const navClose = document.querySelector(".close");
-
 const navLeft = menu.getBoundingClientRect().left;
 navOpen.addEventListener("click", () => {
   if (navLeft < 0) {
@@ -54,7 +52,6 @@ colors.forEach(color => {
 window.addEventListener('scroll', ()=>{
   control.classList.remove('open')
 })
-
 // Scroll To
 const links = [...document.querySelectorAll(".scroll-link")];
 links.map(link => {
@@ -90,25 +87,6 @@ new TypeIt("#type1", {
 gsap.from(".logo", { opacity: 0, duration: 1, delay: 0.5, y: -10 });
 gsap.from(".hamburger", { opacity: 0, duration: 1, delay: 1, x: 20 });
 gsap.from(".banner", { opacity: 0, duration: 1, delay: 1.5, x: -200 });
-gsap.from(".hero h3", { opacity: 0, duration: 1, delay: 2, y: -50 });
-gsap.from(".hero h1", { opacity: 0, duration: 1, delay: 2.5, y: -45 });
-gsap.from(".hero h4", { opacity: 0, duration: 1, delay: 3, y: -30 });
-gsap.from(".hero a", { opacity: 0, duration: 1, delay: 3.5, y: 50 });
-gsap.from(".nav-item", {
-  opacity: 0,
-  duration: 1,
-  delay: 1.2,
-  y: 30,
-  stagger: 0.2,
-});
-gsap.from(".icons span", {
-  opacity: 0,
-  duration: 1,
-  delay: 4,
-  x: -30,
-  stagger: 0.2,
-});
-
 const glide = document.querySelector(".glide");
 if (glide)
   new Glide(glide, {
